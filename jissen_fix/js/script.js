ed5eacbf5ee6545f5553dcd94715fc27b689407e
オオツボワタル <66922846+OWTR@users.noreply.github.com>
2020-07-31 07:45:07 +0900

    ver.12

---
+++ jissen_fix/js/script.js
@@ -0,0 +1,136 @@
+// ハンバーガーメニュー
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+  // 閉じるボタンで、メニューもとじさせる
+  $('.globalMenuSp').click(function(){
+    $('.hamburger, .globalMenuSp').removeClass('active');
+  });
+});
+
+// アコーディオン
+$(function () {
+  //中のコンテンツを隠す
+  $(".section9__accordion__content").css("display", "none");
+  //タイトルがクリックされたら
+  $(".js__accordion__title").click(function () {
+    //クリックしたjs-accordion-title以外の全てのopenを取る
+    $(".js__accordion__title").not(this).removeClass("open");
+    //クリックされたtitle以外のcontentを閉じる
+    $(".js__accordion__title").not(this).next().slideUp(300);
+    //thisにopenクラスを付与
+    $(this).toggleClass("open");
+    //thisのcontentを展開、開いていれば閉じる
+    $(this).next().slideToggle(300);
+  });
+});
+
+// 追従ボタン
+$(function() {
+  var topBtn = $('.following');
+  //ボタンを非表示にする
+  topBtn.hide();
+  //スクロールしてページトップから500に達したらボタンを表示
+  $(window).scroll(function () {
+  if ($(this).scrollTop() > 500) {
+//フェードインで表示
+  topBtn.fadeIn();
+  } else {
+//フェードアウトで非表示
+  topBtn.fadeOut();
+  }
+  });
+});
+
+// ページ内リンク移動
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    //ヘッダーの高さを取得
+    var header = $('header').height();
+    //ヘッダーの高さを引く
+    var position = target.offset().top - header;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+// スライダー
+function slider(condition) {
+  var current = $(".item.current"),
+      next,
+      index;
+  if (condition >= 0) {
+    next = $(".item").eq(condition);
+    index = condition;
+  } else if (condition == 'prev') {
+    next = current.is(":first-child") ? $(".item").last() : current.prev();
+    index = next.index();
+  }else {
+    next = current.is(":last-child") ? $(".item").first() : current.next();
+    index = next.index();
+  }
+  next.addClass("current");
+  current.removeClass("current");
+  $(".dot").eq(index).addClass("current").siblings().removeClass("current");
+}
+var setSlider = setInterval(slider, 4000);
+
+$(".button").on("click", function() {
+  clearInterval(setSlider);
+  var flag = $(this).is(".prev") ? 'prev' : 'next';
+  slider(flag);
+  setSlider = setInterval(slider, 4000);
+});
+
+$(".dot").on("click", function() {
+  if ($(this).is(".current")) return;
+  clearInterval(setSlider);
+  var index = $(this).index();
+  slider(index);
+  setSlider = setInterval(slider, 4000);
+});
+
+
+// モーダル
+$(function(){
+  $('.js-modal-open').on('click',function(){
+      $('.js-modal').fadeIn();
+      return false;
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      return false;
+  });
+});
+
+// パラメータ取得
+// window.location.search = "?modal=show";
+
+function getUrlQueries() {
+  var queryStr = window.location.search.slice(1);  // 文頭?を除外
+      queries = {};
+
+  // クエリがない場合は空のオブジェクトを返す
+  if (!queryStr) {
+    return queries;
+  }
+
+  // クエリ文字列を & で分割して処理
+  queryStr.split('&').forEach(function(queryStr) {
+    // = で分割してkey,valueをオブジェクトに格納
+    var queryArr = queryStr.split('=');
+    queries[queryArr[0]] = queryArr[1];
+  });
+
+  return queries;
+}
